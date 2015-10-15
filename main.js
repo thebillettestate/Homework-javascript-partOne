a9e7db622bd837b43bfec59071a878b5f5f16f5f
Jon Billett <jon@getsidecar.com>
2015-10-15 16:18:24 -0400

    Homework complete

---
+++ main.js
@@ -0,0 +1,24 @@
+//-------------- Simple alert that creates an annoying pop-up ---------------\\
+
+alert("Let's Get it Started!");
+
+console.log("My Content");
+
+var x = 100;
+var y = 66;
+console.log(x * 40 + y);
+
+var firstName = "Jon ";
+var lastName = "Billett";
+console.log(firstName + lastName);
+
+var favorite_breweries = ["Bell's", "Tired Hands", "Forst and Main", "Carton"];
+console.log(favorite_breweries);
+console.log(favorite_breweries[1])
+
+// var favorite_beers = [ ["Bells", "Two Hearted Ale"], ["Tired Hands", "HopHands"], ["Forest and Main", "Solaire"], ["Carton", "Boat Beer"]];
+// console.log(favorite_beers[1][1]);
+
+var favorite_beers = {brewery: 'Bells', beer:'Two Hearted Ale'};
+console.log(favorite_beers['brewery']);
+console.log(favorite_beers.beer);
