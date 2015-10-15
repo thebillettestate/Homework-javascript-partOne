76bb9d0c9aee53d5905f816f7f4fd4913fa21115
Jon Billett <jon@getsidecar.com>
2015-10-15 16:22:12 -0400

    Whoops, I mean now my Homework is complete

--- main.js
+++ main.js
@@ -2,22 +2,31 @@
 
 alert("Let's Get it Started!");
 
+//-------------- Simple conosle alert ---------------\\
+
 console.log("My Content");
+
+//-------------- Add two integer variables together ---------------\\
 
 var x = 100;
 var y = 66;
 console.log(x * 40 + y);
 
+//-------------- Add two string variables together ---------------\\
+
 var firstName = "Jon ";
 var lastName = "Billett";
 console.log(firstName + lastName);
+
+//-------------- Create an array and log different items in it ---------------\\
 
 var favorite_breweries = ["Bell's", "Tired Hands", "Forst and Main", "Carton"];
 console.log(favorite_breweries);
 console.log(favorite_breweries[1])
 
-// var favorite_beers = [ ["Bells", "Two Hearted Ale"], ["Tired Hands", "HopHands"], ["Forest and Main", "Solaire"], ["Carton", "Boat Beer"]];
-// console.log(favorite_beers[1][1]);
+
+
+//-------------- Create a multi-dimensional array and log different items in it ---------------\\
 
 var favorite_beers = {brewery: 'Bells', beer:'Two Hearted Ale'};
 console.log(favorite_beers['brewery']);
